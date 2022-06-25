eb2d723a62adfb6e576a14cdd8805b747e0bc2f1
unknown <wallace_wolv@hotmail.com>
2022-06-25 02:03:41 -0300

    feat: Adicinado imagem react no Subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -26,7 +26,7 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto absolute">
         <div className="max-w-[640px]">
           <Logo />
           <h1 className="mt-8 text-[2.5rem] leading-tight">
@@ -67,6 +67,8 @@ export function Subscribe() {
           </form>
         </div>
       </div>
+
+      <img src="/src/assets/React-js.svg" className="relative m-auto mt-6 mb-[-2rem]" />
 
       <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
     </div>
